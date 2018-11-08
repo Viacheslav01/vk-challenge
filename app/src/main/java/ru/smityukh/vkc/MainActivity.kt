e10ab4a102ddd93b54e4f0e4e676162c73d77bf0
Viacheslav Smityukh <smityukh@yandex-team.ru>
2018-11-08 22:36:44 +0300

    Initial commit

---
+++ app/src/main/java/ru/smityukh/vkc/MainActivity.kt
@@ -0,0 +1,30 @@
+/*
+ * MainActivity.kt
+ *
+ * This file is a part of the Yandex Search for Android project.
+ *
+ * (C) Copyright 2018 Yandex, LLC. All rights reserved.
+ *
+ * Author: Viacheslav Smityukh <smityukh@yandex-team.ru>
+ */
+
+package ru.smityukh.vkc
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+public class AClass {
+
+}
+
+public class BClass {
+    
+}
